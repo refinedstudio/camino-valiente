d34e993d11ccd801b5b3958f247293466bd159b5
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-05-12 00:50:34 -0500

    ✨ feat: add Tailwind CSS support with custom fonts and global styles refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  integrations: [
+    tailwind(),
+    react()
+  ]
 });
